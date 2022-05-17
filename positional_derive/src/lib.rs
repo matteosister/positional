14b697226af9d13a6d6df247ab71cda461febc2d
Matteo Giachino <matteog@gmail.com>
2022-05-17 15:33:34 +0200

    no idea

--- positional_derive/src/lib.rs
+++ positional_derive/src/lib.rs
@@ -1,65 +1,132 @@
-use darling::{ast, util, FromDeriveInput, FromField};
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::parse::{Parse, ParseStream};
-use syn::{parse, parse_macro_input, Attribute, Data, DeriveInput, NestedMeta};
-use syn::{Ident, Type};
+use quote::{format_ident, quote, ToTokens, TokenStreamExt};
+use std::str::FromStr;
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Lit, Meta, NestedMeta, Path};
 
-#[derive(Debug, FromField)]
-#[darling(attributes(positional))]
-struct PositionalRowField {
-    ident: Option<Ident>,
-    ty: Type,
-    #[darling(default)]
-    skip: bool,
+// Generate a compile error to output struct name
+#[proc_macro_derive(PositionalRow, attributes(positional))]
+pub fn positional_row(tokens: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(tokens as DeriveInput);
+    let type_name = ast.ident;
+    let mut all_fields = vec![];
+
+    match ast.data {
+        Data::Struct(data_struct) => {
+            match data_struct.fields {
+                Fields::Named(fields) => {
+                    for field in fields.named {
+                        // it's ok to unwrap here, because we are in a Struct with named fields
+                        let field_ident = field.ident.unwrap();
+                        for attr in field.attrs {
+                            if attr.path.is_ident("positional") {
+                                let meta = attr.parse_meta().expect("unable to parse meta");
+                                let mut attrs = vec![];
+                                parse_meta(&mut attrs, meta);
+                                let row_attributes = create_row_attributes(attrs);
+                                let size = row_attributes.size.to_string();
+                                let filler = row_attributes.filler;
+                                let output = quote! {
+                                    PositionalField::new(&self.#field_ident, #size, #filler)
+                                };
+                                dbg!(&output);
+                                all_fields.push(output);
+                            }
+                        }
+                    }
+                }
+                Fields::Unnamed(_) => {
+                    panic!("only structs with named fields! This is an unnamed struct")
+                }
+                Fields::Unit => panic!("only structs with named fields! This is a unit struct"),
+            }
+        }
+        Data::Enum(_) => panic!("only structs! This is an enum"),
+        Data::Union(_) => panic!("only structs! This is a union type."),
+    };
+
+    quote! {
+        impl PositionalRow for #type_name {
+            fn to_positional_row(&self) -> String {
+                let out = vec![#(#all_fields),*];
+                out.join("")
+            }
+        }
+    }
+    .into()
 }
 
-#[derive(Debug, FromDeriveInput)]
-#[darling(attributes(positional), supports(struct_named))]
-struct PositionalRow {
-    size: i32,
-    filler: String,
+fn parse_meta(attrs: &mut Vec<(Path, Lit)>, meta: Meta) {
+    match meta {
+        Meta::Path(_) => {}
+        Meta::List(meta_list) => {
+            for nested_meta in meta_list.nested {
+                match nested_meta {
+                    NestedMeta::Meta(name_value) => parse_meta(attrs, name_value),
+                    NestedMeta::Lit(_) => {}
+                }
+            }
+        }
+        Meta::NameValue(name_value) => attrs.push((name_value.path, name_value.lit)),
+    }
 }
 
-// // Generate a compile error to output struct name
-// #[proc_macro_derive(PositionalRow, attributes(positional))]
-// pub fn positional_row(tokens: TokenStream) -> TokenStream {
-//     //dbg!(&tokens);
-//     // convert the input tokens into an ast, specially from a derive
-//     let ast = parse_macro_input!(tokens as DeriveInput);
-//
-//     //panic!("My struct name is: <{}>", ast.ident.to_string());
-//
-//     let type_name = ast.ident;
-//
-//     match ast.data {
-//         Data::Struct(data_struct) => {
-//             for field in data_struct.fields {
-//                 let mut nested_meta = vec![];
-//                 for attr in field.attrs {
-//                     if attr.path.is_ident("positional") {
-//                         dbg!(attr.parse_args::<Positional>());
-//                     }
-//                     //nested_meta.push(NestedMeta::from(attr.parse_meta().unwrap()));
-//                 }
-//                 let args = match Positional::from_list(&nested_meta) {
-//                     Ok(v) => v,
-//                     Err(e) => {
-//                         return TokenStream::from(e.write_errors());
-//                     }
-//                 };
-//             }
-//         }
-//         Data::Enum(_) => panic!("only structs! This is an enum"),
-//         Data::Union(_) => panic!("only structs! This is a union type."),
-//     };
-//
-//     quote! {
-//         impl PositionalRow for #type_name {
-//             fn to_positional_row(&self) -> String {
-//                 "test".to_string()
-//             }
-//         }
-//     }
-//     .into()
-// }
+enum FieldAlignment {
+    Left,
+    Right,
+}
+
+impl FromStr for FieldAlignment {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "left" => Ok(Self::Left),
+            "l" => Ok(Self::Left),
+            "right" => Ok(Self::Right),
+            "r" => Ok(Self::Right),
+            _ => Err(format!(
+                "align value should be 'left' or 'right', {} provided",
+                s
+            )),
+        }
+    }
+}
+
+struct RowAttributes {
+    size: usize,
+    filler: char,
+    align: FieldAlignment,
+}
+
+fn create_row_attributes(attrs: Vec<(Path, Lit)>) -> RowAttributes {
+    let mut size = 10;
+    let mut filler = ' ';
+    let mut align = FieldAlignment::Left;
+    for (path, lit) in attrs {
+        if path.is_ident("size") {
+            size = match &lit {
+                Lit::Int(lit_int) => lit_int.base10_parse().unwrap(),
+                _ => panic!("size should be an int"),
+            };
+        }
+
+        if path.is_ident("filler") {
+            filler = match &lit {
+                Lit::Char(lit_char) => lit_char.value(),
+                _ => panic!("filler should be a char"),
+            };
+        }
+
+        if path.is_ident("align") {
+            align = match &lit {
+                Lit::Str(lit_str) => lit_str.value().parse().unwrap(),
+                _ => panic!("align should be either left or right"),
+            };
+        }
+    }
+    RowAttributes {
+        size,
+        filler,
+        align,
+    }
+}
