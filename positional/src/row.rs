ba7e88bbe7914319baccbdeec7f969f571c89dd1
Matteo Giachino <matteog@gmail.com>
2022-05-18 19:21:54 +0200

    multiple macros

--- positional/src/row.rs
+++ positional/src/row.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
 
 /// this trait is implemented by the PositionalRow macros from the positional_derive crate
-pub trait PositionalRow {
+pub trait ToPositionalRow {
     fn to_positional_row(&self) -> String;
+}
+
+pub trait FromPositionalRow {
     fn from_positional_row(row: impl ToString) -> Result<Self, Box<dyn Error>>
     where
         Self: Sized;
